6cbba488bc4c4e209878e065c242059dbec0e8df
kugurerdem <k.ugurerdem@gmail.com>
2024-06-20 23:15:53 +0300

    Initialize Nix home-manager config
    Configured:
    
    - dwm & dwmblocks, .xinitrc
    - bash, .bashrc, .profile, .inputrc files
    - alacritty
    - neovim
    - .gitconfig

---
+++ home-manager/home.nix
@@ -0,0 +1,102 @@
+{ config, pkgs, ... }:
+
+let
+    dwm = pkgs.dwm.overrideAttrs {
+        src = fetchGit {
+            url = "https://github.com/kugurerdem/dwm.git";
+        };
+    };
+
+    dwmblocks = pkgs.dwmblocks.overrideAttrs {
+        src = fetchGit {
+            url = "https://github.com/kugurerdem/dwmblocks.git";
+        };
+    };
+
+in
+{
+    imports = [
+        ./neovim.nix
+        ./alacritty.nix
+    ];
+
+    home = {
+        username = "rugu";
+        homeDirectory = "/home/rugu";
+        stateVersion = "24.05";
+        sessionVariables = {
+            EDITOR = "nvim";
+            VISUAL = "nvim";
+            PAGER = "less";
+        };
+        packages = with pkgs; [
+            brave
+
+            zellij
+            git
+            neofetch
+
+            # window manager stuff
+            dwm
+            dwmblocks
+            dmenu
+
+            xclip
+            xsel
+
+            # required by dwmblocks
+            pamixer
+            acpi
+
+            alacritty
+
+            # Development
+            nodejs_22
+
+            # Shell utilities
+            autojump
+            lf
+        ];
+
+        file.".inputrc".source = ./dotfiles/.inputrc;
+        file.".xinitrc".source = ./dotfiles/.xinitrc;
+        file.".gitconfig".source = ./dotfiles/.gitconfig;
+    };
+
+    programs.bash = {
+        enable = true;
+        shellAliases = {
+            rebuild = "sudo nixos-rebuild switch";
+
+            sudo = "sudo ";
+            ll = "ls -pF";
+            e = "edit_file_opened_by_fzf";
+            diary = ''$EDITOR $HOME/Documents/my/diary/$(date +%G).md'';
+        };
+        bashrcExtra = ''
+            bind -m vi-command 'Control-l: clear-screen'
+            bind -m vi-insert 'Control-l: clear-screen'
+
+            function edit_file_opened_by_fzf() {
+                local file
+                file=$(fzf)
+                if [[ -n $file ]]; then
+                    nvim "$file"
+                fi
+
+            }
+        '';
+        profileExtra = "startx";
+    };
+
+    programs.fzf = {
+        enable = true;
+        enableBashIntegration = true;
+    };
+
+    services.picom = {
+        enable = true;
+        fade = true;
+        fadeSteps = [0.07 0.07];
+    };
+}
---
+++ home-manager/neovim.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ...} :
+
+{
+    programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
+        vimdiffAlias = true;
+
+    extraPackages = with pkgs; [
+        # LSP
+        gopls
+        nodePackages.typescript-language-server
+        vscode-langservers-extracted
+        rust-analyzer
+        lua-language-server
+
+        # Tools
+        go
+    ];
+        plugins = with pkgs.vimPlugins; [
+            vim-commentary
+            vim-airline
+            awesome-vim-colorschemes
+            copilot-vim
+            nvim-lspconfig
+        ];
+    };
+
+    xdg.configFile.nvim.source = ./dotfiles/nvim;
+}

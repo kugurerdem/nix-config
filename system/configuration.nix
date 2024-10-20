c4895bda1d41fb800cb43391c815e35648115234
kugurerdem <k.ugurerdem@gmail.com>
2024-10-20 14:17:03 +0300

    Remove `i18n.extraLocaleSettings`

--- system/configuration.nix
+++ system/configuration.nix
@@ -24,18 +24,6 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "tr_TR.UTF-8";
-    LC_IDENTIFICATION = "tr_TR.UTF-8";
-    LC_MEASUREMENT = "tr_TR.UTF-8";
-    LC_MONETARY = "tr_TR.UTF-8";
-    LC_NAME = "tr_TR.UTF-8";
-    LC_NUMERIC = "tr_TR.UTF-8";
-    LC_PAPER = "tr_TR.UTF-8";
-    LC_TELEPHONE = "tr_TR.UTF-8";
-    LC_TIME = "tr_TR.UTF-8";
-  };
-
   # Configure keymap in X11
   services.xserver = {
     enable = true;

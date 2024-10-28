1ea5b036937494e4e44ff13c896adeec823b236b
kugurerdem <k.ugurerdem@gmail.com>
2024-10-28 13:55:10 +0300

    Add fasd plugin to fish shell

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -156,6 +156,17 @@ in
 
     programs.fish = {
         enable = true;
+        plugins = [
+            {
+                name = "fasd";
+                src = pkgs.fetchFromGitHub {
+                    owner = "oh-my-fish";
+                    repo = "plugin-fasd";
+                    rev = "38a5b6b6011106092009549e52249c6d6f501fba";
+                    sha256 = "06v37hqy5yrv5a6ssd1p3cjd9y3hnp19d3ab7dag56fs1qmgyhbs";
+                };
+            }
+        ];
         shellAliases = {
             sudo = "sudo ";
             ll = "ls -pF";

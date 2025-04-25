660d096b15c2ef53f9f87076102eca5c2eb10db7
kugurerdem <k.ugurerdem@gmail.com>
2025-04-26 00:41:36 +0300

    Fix configuration bug in i3wm.nix

--- home-manager/i3wm.nix
+++ home-manager/i3wm.nix
@@ -1,10 +1,10 @@
 { config, pkgs, ... }: {
     home = {
-        xdg.configFile."i3/config".source = ./dotfiles/.config/i3/config;
-        xdg.configFile."i3blocks/config".source = ./dotfiles/.config/i3blocks/config;
-
         packages = with pkgs; [
           i3blocks
         ];
     };
+
+    xdg.configFile."i3/config".source = ./dotfiles/.config/i3/config;
+    xdg.configFile."i3blocks/config".source = ./dotfiles/.config/i3blocks/config;
 }

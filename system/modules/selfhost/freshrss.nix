6b44f0acd0da25f85197439335f69874a0406e4d
kugurerdem <k.ugurerdem@gmail.com>
2025-05-28 00:35:36 +0300

    feat: Add FreshRSS module configuration

--- system/modules/selfhost/freshrss.nix
+++ system/modules/selfhost/freshrss.nix
@@ -1,9 +1,30 @@
-{lib, ...}: {
-  services.freshrss = {
-    enable = true;
-    defaultUser = "admin";
-    passwordFile = lib.mkDefault /etc/secrets/freshrss-password;
-    baseUrl = lib.mkDefault "freshrss.local";
-    authType = "form";
+{lib, ...}:
+  let cfg = options.selfhosting.freshrss;
+in
+{
+  options.freshrss = {
+    enable = lib.mkEnableOption "FreshRSS";
+    domain = lib.mkOption {
+      type = lib.types.str;
+      default = "freshrss.local";
+      description = "Base URL for FreshRSS instance.";
+    };
   };
+
+  config = {
+    services.freshrss = {
+      enable = lib.mkEnableOption "FreshRSS service";
+      defaultUser = lib.mkDefault "admin";
+      passwordFile = lib.mkDefault "/etc/secrets/freshrss-password";
+      baseUrl = lib.mkDefault "freshrss.local";
+      authType = lib.mkDefault "form";
+    };
+  };
+  # services.freshrss = {
+  #   enable = true;
+  #   defaultUser = "admin";
+  #   passwordFile = lib.mkDefault /etc/secrets/freshrss-password;
+  #   baseUrl = lib.mkDefault "freshrss.local";
+  #   authType = "form";
+  # };
 }
